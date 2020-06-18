28bd4b1773c0d1fe1ce42d08d2ad80c6c1aa23ab
mhbahmani <mhbahmani79@gmail.com>
2020-06-18 20:29:29 +0430

    add Trie class and implement insert method

--- main.swift
+++ main.swift
@@ -14,3 +14,32 @@ class TrieNode {
         children[newChild] = TrieNode(value: newChild, parent: self)
     }
 }
+
+
+class Trie {
+    fileprivate let root: TrieNode
+
+    init() {
+        root = TrieNode()
+    }
+
+    func insert(word: String) {
+    guard !word.isEmpty else { return }
+    var currentNode = root
+
+    let characters = Array(word.characters)
+    var currentIndex = 0
+    while currentIndex < characters.count {
+        let character = characters[currentIndex]
+
+        if let child = currentNode.children[character] {
+            currentNode = child
+        } else {
+            currentNode.add(newChild: character)
+            currentNode = currentNode.children[character]!
+        }
+        currentIndex += 1
+    }
+    currentNode.leafNode = true
+  }
+}
